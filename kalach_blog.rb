8006fa5624a91e52beb90e3c088e159b7d34a753
Guillaume Claret <dev@clarus.me>
2014-11-21 17:55:52 +0100

    Beginning of a pages mechanism

--- kalach_blog.rb
+++ kalach_blog.rb
@@ -1,36 +1,29 @@
 require 'erb'
 include ERB::Util
 
-# Models
 class Blog
-  attr_reader :title, :url, :disqus, :posts
+  attr_reader :title, :url, :disqus, :pages, :posts
   
   def initialize(title, url, disqus)
     @title, @url, @disqus = title, url, disqus
-    @posts = []
-    Dir.foreach("posts") do |file_name|
-      if File.extname(file_name) == ".html"
-        @posts << Post.new(File.basename(file_name, ".html"))
-      end
-    end
-    @posts.sort! {|a, b| - (a.date <=> b.date)}
+    @posts = Dir.glob("posts/*").map {|file_name| Post.new(file_name)}
+      .sort_by {|post| post.date}.reverse
+    @pages = Dir.glob("pages/*")
   end
 end
 
 class Post
   attr_reader :name, :date, :content, :url
   
-  def initialize(name)
-    file_name = "posts/#{name}.html"
-    if /\A(\d+)-(\d+)-(\d+)\s*(.*)\z/ === name
+  def initialize(file_name)
+    if /\A(\d+)-(\d+)-(\d+)\s*(.*)\z/ === File.basename(file_name, ".html") then
       @date = Time.local($1, $2, $3)
       @name = $4
     else
-      @date = Time.at(0)
-      @name = name
+      raise "The name #{file_name.inspect} should have the form \"yyyy-mm-dd title.html\"."
     end
-    @content = File.read(file_name)
-    @url = "#{@name}.html"
+    @content = File.read(file_name, encoding: "UTF-8")
+    @url = "#{@name.gsub(/[^a-zA-Z0-9]/, "-")}.html"
   end
 
   def date_string
@@ -38,24 +31,23 @@ class Post
   end
 end
 
-# Views
-class Template
+class ErbFile
   def initialize(file_name)
-    @erb = ERB.new(File.read("templates/#{file_name}"))
+    @erb = ERB.new(File.read(file_name, encoding: "UTF-8"))
   end
   
   def result(binding)
-    @erb.result(binding).gsub(/^\s*$\n/, "").chomp
+    @erb.result(binding).gsub(/^\s*$\n/, "")
   end
 end
 
 module Helpers
   def header(blog, title, active_link)
-    Template.new("header.rhtml").result(binding)
+    ErbFile.new("templates/header.html.erb").result(binding)
   end
   
   def footer
-    Template.new("footer.rhtml").result(binding)
+    ErbFile.new("templates/footer.html.erb").result(binding)
   end
 end
 
@@ -66,30 +58,26 @@ end
 
 class PageView < View
   def initialize(blog, file_name)
-    extension = File.extname(file_name)
-    @url = "#{File.basename(file_name, extension)}.#{extension[2..-1]}"
-    @html = Template.new(file_name).result(binding)
+    @url = File.basename(file_name, ".erb")
+    @html = ErbFile.new(file_name).result(binding)
   end
 end
 
 class PostView < View
   def initialize(blog, post)
     @url = post.url
-    @html = Template.new("post.rhtml").result(binding)
+    @html = ErbFile.new("templates/post.html.erb").result(binding)
   end
 end
 
-# Controller
-class Controller
+class Renderer
   def initialize(blog)
     @blog = blog
   end
   
-  def make
-    pages = ["index.rhtml", "posts.rhtml", "about.rhtml", "rss.rxml"]
-    page_views = pages.collect {|file_name| PageView.new(@blog, file_name)}
-    post_views = @blog.posts.collect {|post| PostView.new(@blog, post)}
-    
+  def render
+    page_views = @blog.pages.map {|file_name| PageView.new(@blog, file_name)}
+    post_views = @blog.posts.map {|post| PostView.new(@blog, post)}
     (page_views + post_views).each do |view|
       File.open("blog/#{view.url}", "w") {|f| f << view.html}
     end
@@ -97,4 +85,4 @@ class Controller
 end
 
 # Run
-Controller.new(Blog.new(*ARGV)).make
+Renderer.new(Blog.new(*ARGV)).render
